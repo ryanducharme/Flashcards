d8d2abc5628caf40a896a35a6c389c5a05ab0d2a
Alex Lutterloah <alexlutterloah@gmail.com>
2022-10-19 14:02:25 -0400

    Removed server.js console log

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 const sets = [
-  set1 = {
-    name: 'A cool Set Name',
-    cards: [
-      {
-        question: 'What color is the sky?',
-        answer: 'Blue'
-      }
-    ]
-  }
-]
-console.log(sets[0]);
+    (set1 = {
+        name: "A cool Set Name",
+        cards: [
+            {
+                question: "What color is the sky?",
+                answer: "Blue",
+            },
+        ],
+    }),
+];
+
 // app.use(express.static('public'))
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index', sets);
-})
+app.get("/", (req, res) => {
+    res.render("index", sets);
+});
 
-app.get('/newSet', (req, res) => {
-  res.render('newSet', sets);
-})
+app.get("/newSet", (req, res) => {
+    res.render("newSet", sets);
+});
 
-app.post('/newSet', (req, res) => {
-  console.log('posted');
-})
-
+// app.post("/newSet", (req, res) => {
+//     console.log("posted");
+// });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
