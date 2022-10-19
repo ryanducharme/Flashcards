48c219be47a63868d7fec2a2d75a35e895f985d8
Ryan DuCharme <ryan.ducharme77@gmail.com>
2022-10-19 13:57:37 -0400

    Added flash card and ejs functionality

--- server.js
+++ server.js
@@ -2,17 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+const sets = [
+  set1 = {
+    name: 'A cool Set Name',
+    cards: [
+      {
+        question: 'What color is the sky?',
+        answer: 'Blue'
+      }
+    ]
+  }
+]
+console.log(sets[0]);
+// app.use(express.static('public'))
 
 app.set("view engine", "ejs");
 
-app.get('/123', (req, res) => {
-  res.render('index.ejs');
+app.get('/', (req, res) => {
+  res.render('index', sets);
 })
 
-// app.get('/createNewSet', (req, res) => {
-//   res.sendFile(__dirname + '/public/newSet.html');
-// })
+app.get('/newSet', (req, res) => {
+  res.render('newSet', sets);
+})
+
+app.post('/newSet', (req, res) => {
+  console.log('posted');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
